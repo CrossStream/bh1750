185b43f737b1f52111d9e632c85b0dce67e8405b
Philippe Coval <philippe.coval@osg.samsung.com>
2018-03-01 16:15:29 +0100

    Drop uncritical dependencies to enable alt runtimes
    Node is still supported, and IoT.js can be ported.
    
    Change-Id: I58c8c5b728ea6cd457e6618c952c0b7c8a406315
    Signed-off-by: Philippe Coval <philippe.coval@osg.samsung.com>

--- bh1750.js
+++ bh1750.js
@@ -1,31 +1,36 @@
 var i2c = require('i2c');
-var _ = require('lodash');
-var utils = require('./utils');
 
 var BH1750 = function (opts) {
-    this.options = _.extend({}, {
+    this.options = opts || {
         address: 0x23,
         device: '/dev/i2c-1',
         command: 0x10,
         length: 2
-    }, opts);
+    };
     this.verbose = this.options.verbose || false;
     this.wire = new i2c(this.options.address, {device: this.options.device});
 };
 
 BH1750.prototype.readLight = function (cb) {
     var self = this;
-    if (!utils.exists(cb)) {
+    if (!cb) {
         throw new Error("Invalid param");
     }
     self.wire.readBytes(self.options.command, self.options.length, function (err, res) {
-        if (utils.exists(err)) {
+
+        if (err) {
             if (self.verbose)
                 console.error("error: I/O failure on BH1750 - command: ", self.options.command);
             return cb(err, null);
         }
-        var hi = res.readUInt8(0);
-        var lo = res.readUInt8(1);
+        if (Buffer.isBuffer(res)) {
+           var hi = res.readUInt8(0);
+           var lo = res.readUInt8(1);
+        } else {
+           var hi = res[0];
+           var lo = res[1];
+        }
+
         var lux = ((hi << 8) + lo)/1.2;
         if (self.options.command = 0x11) {
             lux = lux/2;
