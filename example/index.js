6ddb73739a5aa20a8086f8f4971c40e85eb94af2
Philippe Coval <p.coval@samsung.com>
2018-06-22 11:38:37 +0200

    WIP: mod: example/index.js (sandbox/rzr/devel/iotjs/master)
    Change-Id: I70568ed357ad90c74d4269a2cc196a976d08c254
    Signed-off-by: Philippe Coval <p.coval@samsung.com>

--- example/index.js
+++ example/index.js
@@ -1,11 +1,13 @@
 var BH1750 = require('../bh1750');
 var light = new BH1750();
 
-light.readLight(function(err, value){
+light.readLight.then((value) => {
     if (err) {
         console.log("light error: " + err);
         throw err;
     } else {
         console.log("light value is: ", value, "lx");
     }
+}).catch((err) => {
+console.log(err);
 });
