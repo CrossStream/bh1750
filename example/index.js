b8eeb9d4d229c9be96fd136f706b560e19cffefa
Philippe Coval <p.coval@samsung.com>
2018-08-08 16:30:24 +0200

    Revert "WIP: mod: example/index.js (sandbox/rzr/devel/iotjs/master)"
    This reverts commit d38af3398f43cd35557b1b21c8c9728941a0272b.

--- example/index.js
+++ example/index.js
@@ -1,13 +1,11 @@
 var BH1750 = require('../bh1750');
 var light = new BH1750();
 
-light.readLight.then((value) => {
+light.readLight(function(err, value){
     if (err) {
         console.log("light error: " + err);
         throw err;
     } else {
         console.log("light value is: ", value, "lx");
     }
-}).catch((err) => {
-console.log(err);
 });
